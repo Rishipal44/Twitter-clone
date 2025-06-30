32936f9392e5f7e868be3f6aa7de47bcd00b1c60
Rishi Pal <rishikumarxyx@gmail.com>
2025-06-30 17:15:13 +0530

    1- user routes and controllers added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             default: []
-        }
+        },
     ],
 
     following: [
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             default: []
-        }
+        },
     ],
 
     profileImg: {
